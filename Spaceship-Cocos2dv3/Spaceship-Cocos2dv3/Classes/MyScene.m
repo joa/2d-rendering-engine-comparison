058d697e8017ad18429a4afe4626b78b60ba4810
Stephan Partzsch <stephan.partzsch@wooga.net>
2014-02-27 17:42:01 +0100

    Added cocos2d version 3 example.

---
+++ Spaceship-Cocos2dv3/Spaceship-Cocos2dv3/Classes/MyScene.m
@@ -0,0 +1,44 @@
+#import "MyScene.h"
+
+
+@implementation MyScene{
+    NSInteger _limit;
+    NSInteger _index;
+    NSInteger _tick;
+}
+
+- (id)init
+{
+    self = [super init];
+    if (self) {
+        _limit = 500;
+        _tick = 0;
+    }
+    return self;
+}
+
+-(void) update: (CCTime)delta{
+    _tick++;
+    
+    if (_tick%4 != 0) {
+        return;
+    }
+    
+    if (_index>=_limit) {
+        return;
+    }
+    
+    CCSprite *sprite = [CCSprite spriteWithImageNamed:@"Spaceship.png"];
+    sprite.position = CGPointMake(arc4random_uniform(self.contentSize.width), arc4random_uniform(self.contentSize.height));
+	sprite.scale = 0.25 / 568 * self.contentSize.width;
+
+    CCActionRotateBy *action = [CCActionRotateBy actionWithDuration:.01 angle:M_PI*2];
+    [sprite runAction:[CCActionRepeatForever actionWithAction:action]];
+    
+    [self addChild:sprite];
+    
+    _index++;
+    
+}
+
+@end
---
+++ Spaceship-Cocos2dv3/Spaceship-Cocos2dv3/Supporting Files/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  Spaceship-Cocos2dv3
+//
+//  Created by Stephan Partzsch on 27/02/14.
+//  Copyright Wooga GmbH 2014. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+int main(int argc, char *argv[]) {
+    
+    @autoreleasepool {
+        int retVal = UIApplicationMain(argc, argv, nil, @"AppDelegate");
+        return retVal;
+    }
+}
